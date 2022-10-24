1104b62b1d70caa474896f332c8956aa472db73a
Imannik <grifann0@gmail.com>
2022-10-24 12:46:02 +0400

    Optimisation

--- views/taskslist_view.php
+++ views/taskslist_view.php
@@ -3,8 +3,34 @@
     if(!isset($_COOKIE['user'])){
         header('Location: /index');
     }
-    //Подключение файла, где будут производиться работа с БД
-    //if(file_exists('tasklisthandler.php')) include 'tasklisthandler.php';
+
+    function draw(){ //Вывод таблицы с задачами на страницу
+        //$items = getItemsFromDB();
+        $model = new Model_Taskslist;
+        $items = $model->getItemsFromDB();
+        while($row = $items->fetch()) {
+            echo '<form action="" method="post">';
+            echo '<input type="hidden" name="id" value="'.htmlspecialchars($row['id']) . '">';
+            echo '<tr>';
+            if($row['status'] == '0'){
+                echo '<td> <button class="btn btn-danger disabled">  </button> </td>';
+            }
+            else{
+                echo '<td> <button class="btn btn-success disabled">  </button> </td>';
+            }
+            echo '<td>' . $row['description'] . '</td>';
+            echo '<td>' . $row['created_at'] . '</td>';
+            if($row['status'] == '0'){
+                echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="ready"><b> READY </b></button></td>';
+            }
+            else{
+                echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="unready"><b> UNREADY </b></button></td>';
+            }
+            echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="delete"><b> DELETE </b></button></td>';
+            echo '</tr>';
+            echo '</form>';    
+        }
+    }
 
 ?>
 <h2> Welcome, <?=$_COOKIE['user']?> </h2>
@@ -19,15 +45,12 @@
                 <button type="submit" class="btn btn-dark" name="add"><b>ADD TASK</b></button>
             </div>
         </div>
-        <div class="row row-col-4">
+        <div class="row row-col-2">
             <div class="col-3">
                 <button type="submit" class="btn btn-outline-dark" name="remove"><b>REMOVE ALL</b> </button>
             </div>
             <div class="col-3">
                 <button type="submit" class="btn btn-outline-dark" name="readyall"><b>READY ALL</b> </button>
-            </div>
-            <div class="col-3">
-                <button type="submit" class="btn btn-outline-dark" name="show"><b>SHOW TASKS</b> </button>
             </div>
         </div>
     </form>
@@ -35,34 +58,6 @@
 <div>
     <table class="table">
     <?php
-        function draw(){ //Вывод таблицы с задачами на страницу
-            //$items = getItemsFromDB();
-            $model = new Model_Taskslist;
-            $items = $model->getItemsFromDB();
-            while($row = $items->fetch()) {
-                echo '<form action="" method="post">';
-                echo '<input type="hidden" name="id" value="'.htmlspecialchars($row['id']) . '">';
-                echo '<tr>';
-                if($row['status'] == '0'){
-                    echo '<td> <button class="btn btn-danger disabled">  </button> </td>';
-                }
-                else{
-                    echo '<td> <button class="btn btn-success disabled">  </button> </td>';
-                }
-                echo '<td>' . $row['description'] . '</td>';
-                echo '<td>' . $row['created_at'] . '</td>';
-                if($row['status'] == '0'){
-                    echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="ready"><b> READY </b></button></td>';
-                }
-                else{
-                    echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="unready"><b> UNREADY </b></button></td>';
-                }
-                echo ' <td class="tabbut"> <button type="submit" class="btn btn-outline-dark" name="delete"><b> DELETE </b></button></td>';
-                echo '</tr>';
-                echo '</form>';    
-            }
-        }
-
         $controller = new Controller_Taskslist();
         $controller->handle();
                   
